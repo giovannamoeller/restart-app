a96cf14536d48dfb510d8921da6d8675b4ce776e
giovannamoeller <giovannagmoeller@gmail.com>
2023-04-18 12:09:42 -0300

    using app storage and creating main logic

--- Restart/Screens/ContentView.swift
+++ Restart/Screens/ContentView.swift
@@ -8,19 +8,20 @@
 import SwiftUI
 
 struct ContentView: View {
-    var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
-        }
-        .padding()
+  @AppStorage("onboarding") var isOnboardingViewActive: Bool = true
+  var body: some View {
+    ZStack {
+      if isOnboardingViewActive {
+        OnboardingView()
+      } else {
+        HomeView()
+      }
     }
+  }
 }
 
 struct ContentView_Previews: PreviewProvider {
-    static var previews: some View {
-        ContentView()
-    }
+  static var previews: some View {
+    ContentView()
+  }
 }
