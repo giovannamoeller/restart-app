ae9e049652de69fadfbd268bfb27b45f18a1a3c3
giovannamoeller <giovannagmoeller@gmail.com>
2023-04-18 00:18:53 -0300

    Initial Commit

---
+++ Restart/RestartApp.swift
@@ -0,0 +1,17 @@
+//
+//  RestartApp.swift
+//  Restart
+//
+//  Created by Giovanna Moeller on 18/04/23.
+//
+
+import SwiftUI
+
+@main
+struct RestartApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
